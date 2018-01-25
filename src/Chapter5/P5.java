6c393162aa87aaf7cd8d9722a964e5cc5f73c4ef
TylerBynum <tb0992562@RK03-B1225-05.francistuttle.edu>
2018-01-25 09:32:19 -0600

    First file version

--- src/Chapter5/P5.java
+++ src/Chapter5/P5.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Chapter5;
+
+import java.util.Scanner;
 
 /**
  * Program to display the output of ('1' + 1 + 1) to console
@@ -18,6 +15,46 @@ public class P5 {
      * @param args arguments from command line prompt
      */
     public static void main(String[] args) {
-        System.out.println('1' + 1 + 1);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 'Y' for a vote yes, enter 'N' for a vote no, or 'Q' to quit voting ");
+        String infadelInput = input.next().toUpperCase();
+
+        int voteYes = 0;
+        int voteNine = 0;
+        int invalid = 0;
+        int total = 0;
+        char x = infadelInput.charAt(0);
+
+        x = infadelInput.charAt(0);
+        if (x == 'Y') {
+            voteYes += 1;
+        } else if (x == 'N') {
+            voteNine += 1;
+        } else if (x != 'Q' && x != 'Y' && x != 'N') {
+            invalid += 1;
+            System.out.println("INVALID, CORRECT VOTE. ");
+        }
+
+        while (x != 'Q') {
+            System.out.println("Enter 'Y' for a vote yes, enter 'N' for a vote no, or 'Q' to quit voting ");
+            infadelInput = input.next().toUpperCase();
+            x = infadelInput.charAt(0);
+            if (x == 'Y') {
+                voteYes += 1;
+            } else if (x == 'N') {
+                voteNine += 1;
+            } else if (x != 'Q' && x != 'Y' && x != 'N') {
+                invalid += 1;
+                System.out.println("INVALID, CORRECT VOTE. ");
+
+            }
+            total += 1;
+        }
+        System.out.println("\nThe voting results are: "
+                + "\n\tYes votes :              " + voteYes + ""
+                + "\n\tVotes no:                " + voteNine + ""
+                + "\n\tInvalid votes:           " + invalid + ""
+                + "\n\tTotal number of votes:   " + total);
+
     }
 }
---
+++ src/Chapter8/C8_1.java
@@ -0,0 +1,107 @@
+package Chapter8;
+
+import java.util.Arrays;
+
+/**
+ * program to display how many hours an employee worked on which days for the week to the console
+ *
+ * @author Tyler Bynum
+ */
+public class C8_1 {
+    /**
+     * Main Method
+     * 
+     * @param args arguments from the command line prompt
+     */
+
+    public static void main(String[] args) {
+
+        int[][] hours = {
+            {2, 4, 3, 4, 5, 8, 8},
+            {7, 3, 4, 3, 3, 4, 4},
+            {3, 3, 4, 3, 3, 2, 2},
+            {9, 3, 4, 7, 3, 4, 1},
+            {3, 5, 4, 3, 6, 3, 8},
+            {3, 4, 4, 6, 3, 4, 4},
+            {3, 7, 4, 8, 3, 8, 4},
+            {6, 3, 5, 9, 2, 7, 9}};
+
+        int[] thing = sum(hours);
+
+        //week hours
+        int[] week = new int[hours.length];
+        for (int i = 0; i < hours.length; i++) {
+            for (int j = 0; j < hours[i].length; j++) {
+                week[i] += hours[i][j];
+            }
+        }
+
+        int[] indexList = new int[week.length];
+
+        sort(week, indexList);
+
+        for (int i = week.length - 1; i >= 0; i--) {
+            System.out.println("Employee" + indexList[i] + " " + week[i]);
+        }
+        System.out.println("\t    Su M  T W  Th F Sa Total  ");
+
+        for (int i = 0; i < hours.length; i++) {
+
+            System.out.print("Employee " + i + ";");
+            System.out.println(Arrays.toString(hours[i]) + " " + thing[i]);
+
+        }
+
+    }
+    /**
+     * Program sorts the list and displays it to the console
+     * @param list decides max list
+     * @param indexList sorts the list from greatest to least
+     */
+
+    public static void sort(int[] list, int[] indexList) {
+        int max;
+        int x_max;
+
+        for (int i = 0; i < indexList.length; i++) {
+            indexList[i] = i;
+        }
+
+        for (int i = list.length - 1; i >= 1; i--) {
+
+            max = list[i];
+            x_max = i;
+
+            for (int j = i - 1; j >= 0; j--) {
+                if (max < list[j]) {
+                    max = list[j];
+                    x_max = j;
+                }
+            }
+
+            if (x_max != i) {
+                list[x_max] = list[i];
+                list[i] = max;
+
+                int temp = indexList[i];
+                indexList[i] = indexList[x_max];
+                indexList[x_max] = temp;
+            }
+        }
+    }
+/**
+ * Program finds the sum of the array length
+ * @param x variable to help sort the list
+ * @return sum of the length
+ */
+    public static int[] sum(int[][] x) {
+        int[] sum = new int[8];
+        for (int i = 0; i < sum.length; i++) {
+            for (int j = 0; j < x[0].length; j++) {
+                sum[i] += x[i][j];
+            }
+        }
+        return sum;
+    }
+
+}
